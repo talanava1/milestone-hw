5e040395ce69f40e8baebd708cf96c81eb7c3edc
Tal Anava <talanava@Tals-MacBook-Pro-5.local>
2022-11-05 21:05:13 +0200

    milestone 5 updated

--- milestone 5/Fibonacci milestone 5.js
+++ milestone 5/Fibonacci milestone 5.js
@@ -1,25 +1,38 @@
 const input = document.getElementById('fib');
 const loading = document.getElementById("loading-container")
 const errImg = document.getElementById("err-img")
+const output = document.getElementById('input')
+
 
 let is42 = false
 
+function showLoading(){
+// removing class atribute making it visible
+loading.removeAttribute("class", "visually-hidden");
+}
+  
+function hideLoading(){
+  // setting the class attribute to hidden 
+  loading.setAttribute("class", "visually-hidden");
+}
 
-function is42Handler(){
-  console.log('42 ran')
-  input.setAttribute('class', 'text-danger')
+//  declaring the handlers 
+function is42Handler(){ 
+  hideLoading();
+  output.setAttribute('class', 'text-danger')
   is42 = true;
 }
 
 // if input.value is > 50 show error
 function greaterThan50(){
+  hideLoading();
   console.log('greater ran')
   input.setAttribute('class', 'text-danger border border-danger')
   errImg.removeAttribute('class', 'visually-hidden')
   errImg.setAttribute('class', 'ms-5 ps-5')
 }
 
-function resetInput(){
+function resetForm(){
   input.removeAttribute('class', 'text-dark border border-danger')
   errImg.setAttribute('class', 'visually-hidden')
   is42 = false;
@@ -27,11 +40,10 @@ function resetInput(){
 
 
 
-
 function showResult(){
-  resetInput();
+  resetForm();
   var buddy = "null"
-  const output = document.getElementById('input')
+  
 
   // if value in input field is greater than 50
   if(input.value === 42){
@@ -43,29 +55,35 @@ function showResult(){
 
   // set value to empty string
   output.textContent = ''
+  showLoading();
 
-  // removing class atribute making it visible
-  loading.removeAttribute("class", "visually-hidden");
 
   fetch(`http://localhost:5050/fibonacci/${input.value}`)
   .then((res) =>{
-    if(res.ok) { return res.json()}
-    return ''
+    if(res.ok) { 
+      return res.json()
+    } else {
+      res.text().then((err) =>{
+        if(input.value == 42){
+          output.textContent = `Server Error: ${err}` //setting the error message in the right place
+          is42Handler(); // Only reutrns syling affects
+        } else if(input.value > 50){
+          greaterThan50();
+          return
+        }
+      })
+    }
   })
   .then(data => {
-    
-    // setting the class attribute to hidden 
-    loading.setAttribute("class", "visually-hidden");
-    if(typeof data === 'string') {
+    if(data === undefined) return
+    hideLoading();
+    if(typeof data === 'string' ) {
       
       output.textContent = data
       return
     }
 
     buddy = data
-    if(is42){
-      output.textContent = `server error ${buddy.result}`
-    }
 
     console.log(buddy)
     output.textContent = buddy.result
@@ -75,6 +93,11 @@ console.log(buddy)
 
 }
 
+// if(input.value == 42){
+//   is42Handler();
+// } else if(input.value > 50){
+//   greaterThan50();
+// }
 
 async function runFetch(){
   spinnerHandler();
